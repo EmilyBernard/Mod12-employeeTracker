133c0909f749eaad8548a0f1ba9c17e5e0d1ffcb
EmilyBernard <bernam2000@hotmail.com>
2023-03-19 14:56:52 -0400

    start of server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-1st commit - file structure
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer'); 
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'Corona20!',
+        database: 'courses_db'
+      },
+      console.log(`Connected to the courses_db database.`)
+    );
